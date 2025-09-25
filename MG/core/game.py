87125a3d204fafe6a44070f655388b4894db7308
pdmillington <pdjmillington@yahoo.co.uk>
2025-09-25 16:49:36 +0100

    small change

--- MG/core/game.py
+++ MG/core/game.py
@@ -7,6 +7,7 @@ Created on Thu Jul 17 20:57:58 2025
 """
 
 from core.player import Player
+from core.m_maker import MarketMaker
 import numpy as np
 
 class Game:
@@ -18,7 +19,8 @@ class Game:
                  rounds=1000, 
                  payoff_scheme=None,
                  price_init=100,
-                 lambda_value=None):
+                 lambda_value=None,
+                 market_maker=None):
         """
         Either pass (num_players, memory) for homogeneous or 
         pass memory_list=[m1, m2, ....mN]for heterogeneous players
@@ -56,8 +58,11 @@ class Game:
         self.players = []
         self.prices = []
         self.price_init = price_init
-        self.prices.append(self.price_init)
+        self.prices= [self.price_init]
+        self.returns = []
         self.lambda_value = lambda_value
+        self.market_maker = market_maker
+        self.mm = None
         
         if memory_list is not None:
             # For each value of memory in memory_list, create num_players agents
@@ -73,6 +78,23 @@ class Game:
         
         self.num_players = len(self.players)
         
+        self.mm= MarketMaker() if market_maker is not None else None
+        
+    def _reset_for_run(self):
+        self.history = list(np.random.choice([-1, 1], size=self.memory))
+        self.prices = [self.price_init]
+        self.returns = []
+        self.action = []
+        if self.mm:
+            self.mm.position = 0
+            self.mm.position_by_round = []
+            
+        for p in self.players:
+            p.position = 0
+            p.position_history = []
+            p.order_history = []
+            
+            # p.reset_strategy_scores()
 
     def play_round(self):
         payoff = self.payoff_scheme
@@ -103,27 +125,33 @@ class Game:
         self.returns = []
         
         eps_t = 0.0  # or np.random.normal(0, self.noise_std)
-        r_t =  flow / self.lambda_value + eps_t
+        total_flow = flow - self.mm.position
+        r_t =  flow / self.lambda_value - self.mm.position / (3 * self.lambda_value) + eps_t
         p_next = self.prices[-1] * np.exp(r_t)
         delta_price = p_next - self.prices[-1]
         self.prices.append(p_next)
         self.returns.append(r_t)
+        self.mm.position -= flow
+        self.mm.position_by_round.append(-flow)
+        
+        
 
         # 3) Payoffs update (MG: immediate; Dollar: delayed)
         for p in self.players:
             # MG update: (player, all_actions, flow, history_slice)
             # Dollar update expects delta_price as kw; MG will ignore unknown kw
             if expects_delta_price:
-                self.payoff_scheme.update(p, all_actions, flow, self.history[-p.memory:], self.prices[-1], delta_price=delta_price)
+                self.payoff_scheme.update(p, all_actions, total_flow, self.history[-p.memory:], self.prices[-1], delta_price=delta_price)
             else:
-                self.payoff_scheme.update(p, all_actions, flow, self.history[-p.memory:], self.prices[-1])
+                self.payoff_scheme.update(p, all_actions, total_flow, self.history[-p.memory:], self.prices[-1])
 
 
         # 5) Update “public info” bit for the MG history. Define it on the same 'flow'
-        minority_action = -1 if flow > 0 else 1
+        minority_action = -1 if total_flow > 0 else 1
         self.history = (self.history + [minority_action])[-self.memory:]
         
 
     def run(self):
+        self._reset_for_run()
         for _ in range(self.rounds):
             self.play_round()
